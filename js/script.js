e25de5486500bcc4dd8cbaa7b8856983155742dd
Arpad Kiss <kissarpico22@gmail.com>
2024-05-09 17:01:41 +0200

    Mejorado accesibilidad en modo claro e idiomas (es,en)

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
     let typed = new Typed('#element', {
-        strings: ['Desarrollador', 'Curioso', "Creativo"],
+        strings: ['Desarrollador', 'Curioso', 'Creativo'],
         typeSpeed: 50,
         loop: true
     });
@@ -37,10 +37,73 @@ document.addEventListener("DOMContentLoaded", function () {
             loop: true
         });
     });
+    document.querySelector('#es-flag').addEventListener('click', function () {
+        typed.destroy();
+        typed = new Typed('#element', {
+            strings: ['Desarrollador', 'Curioso', 'Creativo'],
+            typeSpeed: 50,
+            loop: true
+        });
+    });
     fetch('../languages/en.json')
         .then(response => response.json())
         .then(translations => {
             document.querySelector('#uk-flag').addEventListener('click', () => {
+                // Barra de navegación
+                document.querySelector('#navbar-home span').textContent = translations.navbar.home;
+                document.querySelector('#navbar-education span').textContent = translations.navbar.education;
+                document.querySelector('#navbar-technologies span').textContent = translations.navbar.technologies;
+                document.querySelector('#navbar-contact span').textContent = translations.navbar.contact;
+
+                // Sección de inicio
+                let helloElement = document.querySelector('#hero-hello');
+                let spanContent = helloElement.querySelector('span').outerHTML;
+                helloElement.innerHTML = translations.hero.hello + ' ' + spanContent;
+                document.querySelector('#hero-description').textContent = translations.hero.description;
+                document.querySelector('#hero-resume').textContent = translations.hero.resume;
+                document.querySelector('#hero-contact').textContent = translations.hero.contact;
+
+
+
+                // Sección de educación
+                document.querySelector('#education-title').textContent = translations.education.title;
+                translations.education.timeline.forEach((item, index) => {
+                    document.querySelector(`#education-year-${index}`).textContent = item.year;
+                    document.querySelector(`#education-title-${index}`).textContent = item.title;
+                    document.querySelector(`#education-description-${index}`).textContent = item.description;
+                });
+
+                // Sección de tecnologías
+                document.querySelector('#technologies-title').textContent = translations.technologies.title;
+                document.querySelector('#technologies-frontEnd').textContent = translations.technologies.frontEnd;
+                document.querySelector('#technologies-backEnd').textContent = translations.technologies.backEnd;
+                document.querySelector('#technologies-learning').textContent = translations.technologies.learning;
+                document.querySelector('#technologies-tools').textContent = translations.technologies.tools;
+
+                // Formulario de contacto
+                let contactTitle = document.querySelector('#contact-title');
+                contactTitle.firstChild.textContent = translations.contact.title + ' ';
+                document.querySelector('#contact-description').textContent = translations.contact.description;
+                document.querySelector('#contact-form-name').setAttribute('placeholder', translations.contact.form.name);
+                document.querySelector('#contact-form-email').setAttribute('placeholder', translations.contact.form.email);
+                document.querySelector('#contact-form-subject').setAttribute('placeholder', translations.contact.form.subject);
+                document.querySelector('#contact-form-message').setAttribute('placeholder', translations.contact.form.message);
+                document.querySelector('#contact-form-button').textContent = translations.contact.form.button;
+
+                // Pie de página
+                let footerElement = document.querySelector('#footer-createdBy');
+                let spanElements = footerElement.querySelectorAll('span');
+                spanElements[0].textContent = translations.footer.site;
+
+                let textNode = footerElement.childNodes[1];
+                textNode.textContent = ' ' + translations.footer.createdBy + ' ';
+                document.querySelector('#small').textContent = translations.footer.partiallyCreated;
+            });
+        });
+    fetch('../languages/es.json')
+        .then(response => response.json())
+        .then(translations => {
+            document.querySelector('#es-flag').addEventListener('click', () => {
                 // Barra de navegación
                 document.querySelector('#navbar-home span').textContent = translations.navbar.home;
                 document.querySelector('#navbar-education span').textContent = translations.navbar.education;
